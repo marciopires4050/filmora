7a651d1872fb09607fb877c69aa9ed4b19877f48
marciopires4050 <mpbox80@gmail.com>
2021-12-01 17:33:21 -0300

    Initial commit

---
+++ src/components/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="loading">
+    <h3>Carregando dados...</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h3 {
+  font-size: 25px;
+}
+</style>
